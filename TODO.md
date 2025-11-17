d8785c208213ba2f5166f435ff986b0666aa6207
Atqa Munzir <atqamz@gmail.com>
2025-11-18 00:08:33 +0700

    add more todo to populate dnf-fresh.sh

--- TODO.md
+++ TODO.md
@@ -6,4 +6,4 @@
 - [ ] change default file dialog, check first current option. I want to replace file manager fully with yazi, can we also use it for file dialogs?
 - [ ] change swaylock background to black
 - [ ] change sddm background to black
-- [ ] 
+- [ ] populate dnf-fresh
--- dnf-fresh.sh
+++ dnf-fresh.sh
@@ -6,4 +6,12 @@
 # set /etc/dnf/dnf.conf to include fastestmirror and max_parallel_downloads
 
 
+
+# copr repos
+sudo dnf copr enable alternateved/cliphist -y
+
+# update system
 sudo dnf upgrade --refresh -y
+
+# install packages
+sudo dnf install cliphist wl-copy wl-paste pavucontrol wl-clipboard wtype grim slurp -y
