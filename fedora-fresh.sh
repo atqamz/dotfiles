19520f033e40f29d8b9a9d95f00818d9df1e807e
Atqa Munzir <atqamz@gmail.com>
2025-12-07 11:59:33 +0700

    fedora-fresh: add vscode

--- fedora-fresh.sh
+++ fedora-fresh.sh
@@ -181,3 +181,8 @@ sudo dnf install \
 # unityhub
 sudo sh -c 'echo -e "[unityhub-beta]\nname=Unity Hub Beta\nbaseurl=https://hub.unity3d.com/linux/repos/rpm/unstable\nenabled=1\ngpgcheck=1\ngpgkey=https://hub.unity3d.com/linux/repos/rpm/unstable/repodata/repomd.xml.key\nrepo_gpgcheck=1" > /etc/yum.repos.d/unityhub_beta.repo'
 sudo dnf install unityhub
+
+# vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+sudo dnf install code -y
